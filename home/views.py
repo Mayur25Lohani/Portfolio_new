627827bf0067e597ea56db5522843d3bfc12eeba
unknown <nabhya.khoria@kgpian.iitkgp.ac.in>
2023-03-25 14:20:25 +0530

    Files added - 1

--- home/views.py
+++ home/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def profiles(request):
+    return render(request, 'team.html')
+
+
