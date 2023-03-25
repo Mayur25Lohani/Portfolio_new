4761e73bf087e956ad9c67b740fcd2d8162a1683
unknown <nabhya.khoria@kgpian.iitkgp.ac.in>
2023-03-25 16:27:41 +0530

    Models done

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path("", views.index, name='home'),
     path("index.html", views.index, name='home'),
     path("team.html", views.profiles, name='team'),
     path("contact.html", views.contact, name='contact')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
