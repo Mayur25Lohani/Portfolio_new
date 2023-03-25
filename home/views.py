83952923e3c9729d4e47fa10dd6d8ed9f30bc3c4
unknown <nabhya.khoria@kgpian.iitkgp.ac.in>
2023-03-25 16:43:10 +0530

    Model done and integrated

--- home/views.py
+++ home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from home import models
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def profiles(request):
-    return render(request, 'team.html')
+    mentor_profiles = models.Mentor_Profile.objects.all()
+    return render(request, 'team.html', {'mentor_profiles':mentor_profiles,})
 
 def contact(request):
     return render(request, 'contact.html')
