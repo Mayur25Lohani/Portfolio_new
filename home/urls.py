76119930a0d2d5e1949b19cff1e17749f99e3f94
unknown <nabhya.khoria@kgpian.iitkgp.ac.in>
2023-03-31 18:27:52 +0530

    Search Function enabled

--- home/urls.py
+++ home/urls.py
@@ -4,10 +4,12 @@ from home import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+app_name = 'home'
 
 urlpatterns = [
     path("", views.index, name='home'),
     path("index.html", views.index, name='home'),
     path("team.html", views.profiles, name='team'),
-    path("contact.html", views.contact, name='contact')
+    path("contact.html", views.contact, name='contact'),
+    path("search",views.profiles_search, name="profile_search"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -11,3 +11,17 @@ def profiles(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def profiles_search(request):
+    query = request.GET.get('query')
+
+    mentors_list = models.Mentor_Profile.objects.filter(short_description__contains=query)
+    mentors_list = mentors_list.union(models.Mentor_Profile.objects.filter(long_description__contains=query))
+    mentors_list = mentors_list.union(models.Mentor_Profile.objects.filter(department__contains=query))
+    mentors_list = mentors_list.union(models.Mentor_Profile.objects.filter(role__contains=query))
+    mentors_list = mentors_list.union(models.Mentor_Profile.objects.filter(username__first_name__contains=query))
+    mentors_list = mentors_list.union(models.Mentor_Profile.objects.filter(username__last_name__contains=query))
+    mentors_list = mentors_list.union(models.Mentor_Profile.objects.filter(hall__contains=query))
+
+    return render(request,'team.html',{'query_profiles':mentors_list,})
